c2c184f1a4c02ffb3969f1a5d2b61180f801d953
AvasBrahma <avasbrahma@gmail.com>
2022-12-19 23:56:22 +0530

    modification done style and html and js

--- calScript.js
+++ calScript.js
@@ -1,1 +1,45 @@
-var buttons=document.getElementsByClassName("button");
+var buttons=document.getElementsByClassName("input-keys");
+
+var display=document.getElementById("display");
+
+var operand1=0;
+var operand2=null;
+var operator=null;
+var result=null;
+
+for(var i=0; i<buttons.length;i++){
+    buttons[i].addEventListener('click', function(){
+        var value=this.getAttribute('data-value');
+
+        if(value=='+')//if operator is click
+        {
+          operator='+';
+          operand1=parseFloat(display.textContent);
+
+        } else if(value=='-')
+        {
+          operator='+';
+          operand1=parseFloat(display.textContent);
+
+        }else if(value=='*'){
+           operator='+';
+          operand1=parseFloat(display.textContent);
+
+        }else if(value=='/'){
+          operator='/';
+          operand1=parseFloat(display.textContent);
+
+        }else if(value=='='){
+            
+            operand2=parseFloat(display.textContent);
+            // used eval to get result
+            //show result on display
+          result=eval(operand1+""+operator+""+operand2)
+          display.innerText +=result;
+  
+          }else{
+            display.innerText +=value;
+        }
+
+    })
+}
