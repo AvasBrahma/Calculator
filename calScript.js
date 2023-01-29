1693171b86c6e4c71b1dd3db5c6db394120673c9
AvasBrahma <avasbrahma@gmail.com>
2023-01-29 23:27:04 +0530

    modication done in all three files

--- calScript.js
+++ calScript.js
@@ -1,45 +1,100 @@
-var buttons=document.getElementsByClassName("input-keys");
+var display = document.getElementById("display");
+var buttons = document.getElementsByClassName("buttons");
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
 
-var display=document.getElementById("display");
+// Input through Calculator buttons
 
-var operand1=0;
-var operand2=null;
-var operator=null;
-var result=null;
+for(var i = 0; i< buttons.length ; i++){
+    buttons[i].addEventListener("click", function (){
+        var value = this.getAttribute('data-value');
 
-for(var i=0; i<buttons.length;i++){
-    buttons[i].addEventListener('click', function(){
-        var value=this.getAttribute('data-value');
-
-        if(value=='+')//if operator is click
-        {
-          operator='+';
-          operand1=parseFloat(display.textContent);
-
-        } else if(value=='-')
-        {
-          operator='+';
-          operand1=parseFloat(display.textContent);
-
-        }else if(value=='*'){
-           operator='+';
-          operand1=parseFloat(display.textContent);
-
-        }else if(value=='/'){
-          operator='/';
-          operand1=parseFloat(display.textContent);
-
-        }else if(value=='='){
-            
-            operand2=parseFloat(display.textContent);
-            // used eval to get result
-            //show result on display
-          result=eval(operand1+""+operator+""+operand2)
-          display.innerText +=result;
-  
-          }else{
-            display.innerText +=value;
+        if(value == 'reset') {
+            display.innerText = null;
         }
 
-    })
+        else if(value == '+' || value == '-' || value == "*" || value == "/" || value == "%"){
+        
+                operator = value;
+                operand1 = parseFloat(display.innerText);
+                console.log("opera 1 = ",operand1);
+                display.innerText = null;
+        }
+
+        else if(value == '='){            
+            if(operator != null){
+                operand2 = parseFloat(display.innerText);
+                display.innerText = eval(operand1+" "+operator+" "+operand2);
+            }
+            else{
+                display.innerText = "Error";
+            }
+        }
+
+        else if(value == 'sign'){
+            display.innerText = eval(parseFloat(display.innerText) * (-1) );
+        }
+
+        else if(value == '.') {
+            if(display.innerText.length && !display.innerText.includes('.') ) {
+                display.innerText += value;
+            }
+        }
+        else{
+            display.innerText += value;
+        }
+
+    });
 }
+
+
+// Input with Keys
+
+document.addEventListener("keypress", function(event) {
+    var key;
+    key =  event.keyCode;
+    value = String.fromCharCode(key);
+    console.log(key+" "+value);
+    var numArray = [1,2,3,4,5,6,7,8,9,0];
+
+
+    if(value == '+' || value == '-' || value == "*" || value == "/" || value == "%"){
+    
+            operator = value;
+            operand1 = parseFloat(display.innerText);
+            display.innerText = null;
+    }
+
+    else if(key == '13'){
+        if(operator != null){
+            operand2 = parseFloat(display.innerText);
+            display.innerText = eval(operand1+" "+operator+" "+operand2);
+        }
+        else{
+            display.innerText = "Error";
+        }
+    }
+
+    else if(value == '.') {
+        if(display.innerText.length && !display.innerText.includes('.') ) {
+            display.innerText += value;
+        }
+    }
+
+    else if(value in numArray){
+        display.innerText += value;
+    }
+})
+
+
+// For Clearing display with delete Key 
+
+document.addEventListener("keydown", function(event) {
+    var key;
+    key =  event.keyCode;
+
+    if(key == '8') {
+        display.innerText = null;
+    }
+});
